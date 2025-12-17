9ab299288d13b9bfe5213ad6eb305ac1770a595d
unknown <tidahks@gmail.com>
2025-12-17 13:46:48 +0900

    upload

--- delete.php
+++ delete.php
@@ -8,7 +8,7 @@ $connect=mysqli_connect("localhost","tidahk","wiatnpfa*8","tidahk");
 $sql1 =" DELETE FROM writedb WHERE count='{$_POST['json3_id_php']}'";
 $query1=mysqli_query($connect,$sql1);
 var_dump($query1);
-
+ 
 
 
 
--- fix.php
+++ fix.php
@@ -8,8 +8,8 @@ $connect=mysqli_connect("localhost","tidahk","wiatnpfa*8","tidahk");
 $sql1 = "UPDATE writedb SET title_db ='{$_POST['fixtitle_php']}'
 ,content_db='{$_POST['fixcontent_php']}' WHERE count ='{$_POST['id_php']}';";
 $query1=mysqli_query($connect,$sql1);
-var_dump($query1)
-/*
+var_dump($query1)  
+/*  
 
 $data=array();
 while($row=mysqli_fetch_array($query1)){
